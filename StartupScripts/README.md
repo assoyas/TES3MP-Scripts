45d10dbe7c16702a8711e3074d60548a6670d422
Scott Warwick <otheremailformemes@gmail.com>
2018-07-08 20:15:48 +0200

    Startup Scripts

---
+++ StartupScripts/README.md
@@ -0,0 +1,50 @@
+## Startup Scripts
+### VERSION `1.0`
+
+**REQUIRES STATESPAM FIX SCRIPT OR ELSE THINGS WILL GET UGLY**
+
+[StateSpam fix can be found here](https://github.com/SkotisVarvikas/TES3MP-Stuffs/tree/master/StateSpam/)
+
+## ABOUT THE SCRIPT
+
+The script attempts to load as much of the missing objects, variables and anything else that would otherwise be lost after a relog. The StateSpam script is required to not cause conflicts when certain objects appear in different states for players currently in a cell. The variables and entities are adjusted based on player's journal and is independent of the journal sharing progress. Current fixes addressed by the script are documented below and one can expect the list to expand in the future.
+
+
+## KNOWN ISSUES
+
+The choice for estate in RavenRock will not save. There is no journal entry to know where YOU decided to build YOUR estate. The location is controlled by `ColonyState ? [34;36]`, where each value denotes different location. Since this is the variable we calculate, there is no way to know where the estate was placed originally. Therefore, you will be eventually forced to use same estate location on the whole server (unless server has custom variables being saved that accounts for this - although in that case there is no need to even use this script). It is a small detail that should not affect many players and definitely has no impact on the gameplay.
+
+Since the Great House strongholds are all controled by a single variable, it is possible for people to access other houses' strongholds and wreak havoc, screwing up several quests for other players. There is no way to avoid this without constantly resetting cells or using instances.
+
+Startup script, which disables a bunch of entities in 60 cells, will not execute if at least one of them is already loaded. Either delete all the cells and relog or, if for some reason one of the cells **has** to remain, delete it from the `startupCells` variable found at the top.
+
+
+## INSTALLING INSTRUCTIONS
+
+1) Copy `startupScripts.lua` to `.../tes3mp/mp-stuff/scripts`.
+
+2) Open `server.lua` with a text editor.
+
+3) Add `startupScripts = require("startupScripts")` at the top, along with all other included scripts.
+
+4) Save `server.lua`, open `stateHelper.lua`.
+
+5) Open `myMod.lua` with a text editor.
+
+6) Find `Methods.OnPlayerConnect = function(pid, playerName)` function. Below it, add `startupScripts.onServerInit(pid)`.
+
+7) Save `myMod.lua`.
+
+8) Find `function StateHelper:LoadJournal(pid, stateObject)` function. At the end of it, you will find `tes3mp.SendJournalChanges(pid)`. Right above it, put a call to this script by adding `startupScripts.onLogin(pid, stateObject)`.
+
+9) Save `stateHeler.lua`. If you have progress in some of the quests addressed by the script as well as console access, you can login to see certain console commands being executed.
+
+## LIST OF FIXES
+
+• RavenRock in Solstheim island, including choice of person you side with, service and few less significant statuses, will be loaded properly.
+• Strongholds built through Great House questlines will persist.
+• A fix for auto-goodbye when talking to NPCs in Mournhold after relogging once the `MournholdAttack` quest has been done.
+• NPCs and several objects disabled by `Startup` script in TES3, which is executed at the very beginning of the game, will be disabled if the server is "fresh" - none of the 60 cells affected by the TES3 script have files created for them yet.
+
+## startupScripts.xlsx
+The excel spreadsheet file contains my findings of how the entities, variables and other things are modified by journal entries and which indexes do so.
---
+++ StartupScripts/startupScripts.lua
@@ -0,0 +1,209 @@
+Methods = {}
+local startupCells = {"0, 22", "0, -9", "1, 24", "-11, 15", "-11, 9", "-12, 11", "15, 5", "17, -6", "2, 23", "-2, -3", "2, 4", "-2, 5", "-2, 6", "-2, -8", "3, 23", "3, 24", "-3, -8", "3, -9", "-4, 12", "4, -9", "-5, -5", "-5, 9", "-6, -5", "6, -7", "8, 15", "-8, 16", "-8, 2", "-8, 3", "9, 15", "-9, 2", "-9, 4", "9, -7", "Ald-ruhn, Gindrala Hleran's House", "Ald-ruhn, Sarethi Manor", "Ald-ruhn, The Rat In The Pot", "Balmora, Balyn Omarel's House", "Balmora, Eight Plates", "Bthuand", "Cavern of the Incarnate", "Ghostgate, Tower of Dusk", "Gnaar Mok, Nadene Rotheran's Shack", "Ilunibi, Soul's Rattle", "Indarys Manor, Berendas' House", "Kora-Dur", "Mamaea, Sanctum of Black Hope", "Nchurdamz, Interior", "Rothan Ancestral Tomb", "Sadrith Mora, Dirty Muriel's Cornerclub", "Sadrith Mora, Tel Naga Great Hall", "Sadrith Mora, Telvanni Council House", "Telasero, Lower Level", "Vivec, Arena Storage", "Vivec, Foreign Quarter Underworks", "Vivec, Hall of Wisdom", "Vivec, Hlaalu Prison Cells", "Vivec, Jeanne; Trader", "Vivec, Milo's Quarters", "Vivec, Ralen Tilvur; Smith", "Vivec, St. Olms Underworks", "Vivec, Telvanni Enchanter"}
+Methods.onServerInit = function(pid)
+    local serverInitialized = false
+    for index,cellDescription in ipairs(startupCells) do
+        LoadedCells[cellDescription] = Cell(cellDescription)
+        LoadedCells[cellDescription].description = cellDescription
+        if LoadedCells[cellDescription]:HasEntry() then
+            LoadedCells[cellDescription]:Load()
+            serverInitialized = true
+        end
+        myMod.UnloadCell(cellDescription)
+    end
+    if serverInitialized == false then
+        myMod.RunConsoleCommandOnPlayer(pid, "Startscript, Startup")
+    end
+end
+
+Methods.onLogin = function(pid, savedData)
+    --default states and such
+    local colonyState = 0
+    local colonySide = 0
+    local colonyService = 0
+    local colonyNord = 0
+    local noteCount = 0
+    local aferguard = 0
+    local gratianguard = 0
+    local garnasguard = 0
+    local stronghold = 0
+    for index, item in pairs(Players[pid].data.inventory) do
+        if tableHelper.containsKeyValue(Players[pid].data.inventory, "refId", "bk_colony_toralf", true) then
+            itemIndex = tableHelper.getIndexByNestedKeyValue(Players[pid].data.inventory, "refId", "bk_colony_toralf")
+            noteCount = Players[pid].data.inventory[itemIndex].count
+        else
+            noteCount = 0
+        end
+    end
+    for index, value2 in pairs(savedData.data.journal) do
+        local journalEntry = savedData.data.journal[index]
+        local quest = journalEntry.quest
+        local questIndex = journalEntry.index
+        ----------------------Raven Rock-------------------------
+        -- handle colony development steps
+        if tostring(quest) == "co_1" and questIndex == 50 then
+            colonyState = 1
+        end
+        if tostring(quest) == "colony_update" and questIndex == 10 then
+            colonyState = 2
+        end
+        if tostring(quest) == "colony_update" and questIndex == 20 then
+            colonyState = 3
+        end
+        if tostring(quest) == "co_3" and questIndex == 70 then
+            colonyState = 4
+        end
+        if tostring(quest) == "colony_update" and questIndex == 30 then
+            colonyState = 5
+        end
+        if (tostring(quest) == "co_6a" and questIndex == 60) or (tostring(quest) == "co_6" and questIndex == 80) then
+            colonyState = 9
+        end
+        if tostring(quest) == "colony_update" and questIndex == 40 then
+            colonyState = 10
+        end
+        if tostring(quest) == "co_7" and (questIndex == 80 or questIndex == 90 or questIndex == 100 or questIndex == 110) then
+            colonyState = 11
+        end
+        if tostring(quest) == "co_9a" and questIndex == 70 then
+            colonyState = 12
+        end
+        if tostring(quest) == "co_11" and (questIndex == 60 or questIndex == 70) then
+            colonyState = 13
+        end
+        if tostring(quest) == "colony_update" and questIndex == 50 then
+            colonyState = 20
+        end
+        if tostring(quest) == "colony_update" and questIndex == 60 then
+            colonyState = 21
+        end
+        if tostring(quest) == "co_13" and questIndex == 5 then
+            colonyState = 22
+        end
+        if tostring(quest) == "co_13a" and questIndex == 10 then
+            colonyState = 22
+        end
+        if tostring(quest) == "co_13" and questIndex == 70 then
+            colonyState = 23
+        end
+        if tostring(quest) == "colony_update" and questIndex == 70 then -- colony_update is not displayed on journal -> completely skip waiting period
+            colonyState = 30
+        end
+        if tostring(quest) == "co_13" and questIndex == 70 then
+            colonyState = 30
+        end
+        --estate building in progress
+        if tostring(quest) == "co_estate" and questIndex == 40 then
+            colonyState = 31 -- or use 32 or 33 for other location
+        end
+        --estate complete
+        if tostring(quest) == "co_estate" and questIndex == 50 then
+            colonyState = 34 -- or use 35 or 36 for other location
+        end
+        --set it to 10 just to be safe, in case player logs out without turning the quest in
+        if tostring(quest) == "co_13" and questIndex == 10 and noteCount > 0 then
+            colonyNord = 10
+        end
+        --smithy/trading outpost progress and choice
+        if tostring(quest) == "co_4" and (questIndex == 30 or questIndex == 60) then
+            colonyService = 1
+        end
+        if tostring(quest) == "co_4" and (questIndex == 40 or questIndex == 70) then
+            colonyService = 2
+        end
+        if tostring(quest) == "co_4" and questIndex == 80 then
+            colonyService = 3
+        end
+        if tostring(quest) == "co_4" and questIndex == 90 then
+            colonyService = 4
+        end
+        if tostring(quest) == "co_4" and questIndex == 100 then
+            colonyService = 5
+        end
+        if tostring(quest) == "co_4" and questIndex == 110 then
+            colonyService = 6
+        end
+        --which person you side with
+        if tostring(quest) == "co_choice" and questIndex == 40 then
+            colonySide = 1
+        end
+        if tostring(quest) == "co_choice" and questIndex == 50 then
+            colonySide = 2
+        end
+        --extra variables for guards
+        if tostring(quest) == "co_11" and questIndex == 30 then
+            aferguard = 1
+        end
+        if tostring(quest) == "co_11" and questIndex == 40 then
+            gratianguard = 1
+        end
+        if tostring(quest) == "co_11" and questIndex == 50 then
+            garnasguard = 1
+        end
+
+        -------------Startup script--------------------
+
+        -- Startup script related entities being re-enabled based on journal entries
+        -- current approach has issues
+
+        ---------------Strongholds----------------------
+
+        local strongholdQuests = { "hh_stronghold", "hr_stronghold", "ht_stronghold" }
+        if tableHelper.containsValue(strongholdQuests, quest) and questIndex == 55 then
+            if stronghold < 1 then
+                stronghold = 1
+            end
+        end
+        if tableHelper.containsValue(strongholdQuests, quest) and questIndex == 100 then
+            if stronghold < 2 then
+                stronghold = 2
+            end
+        end
+        if tableHelper.containsValue(strongholdQuests, quest) and questIndex == 170 then
+            if stronghold < 3 then
+                stronghold = 3
+            end
+        end
+        if tableHelper.containsValue(strongholdQuests, quest) and questIndex == 200 then
+            if stronghold < 4 then
+                stronghold = 4
+            end
+        end
+        if tableHelper.containsValue(strongholdQuests, quest) and questIndex == 270 then
+            if stronghold < 5 then
+                stronghold = 5
+            end
+        end
+        if tableHelper.containsValue(strongholdQuests, quest) and questIndex == 300 then
+            if stronghold < 6 then
+                stronghold = 6
+            end
+        end
+
+        ---------------Tribunal fixes-------------------
+
+        if tostring(quest) == "tr_mhattack" and questIndex == 30 then
+            myMod.RunConsoleCommandOnPlayer(pid, "set MournholdAttack to " .. -1)
+        end
+
+        -- more to come
+
+        ----------------Bloodmoon fixes------------------
+
+        -- stuff to come
+    end
+    --set variables/execute scripts via console
+    if colonyState > 0 then
+        myMod.RunConsoleCommandOnPlayer(pid, "startscript, ColonyTimer")
+    end
+    myMod.RunConsoleCommandOnPlayer(pid, "set ColonyState to " .. colonyState)
+    myMod.RunConsoleCommandOnPlayer(pid, "set ColonySide to " .. colonySide)
+    myMod.RunConsoleCommandOnPlayer(pid, "set ColonyService to " .. colonyService)
+    myMod.RunConsoleCommandOnPlayer(pid, "set ColonyNord to " .. colonyNord)
+    myMod.RunConsoleCommandOnPlayer(pid, "set aferguard to " .. aferguard)
+    myMod.RunConsoleCommandOnPlayer(pid, "set gratianguard to " .. gratianguard)
+    myMod.RunConsoleCommandOnPlayer(pid, "set garnasguard to " .. garnasguard)
+    myMod.RunConsoleCommandOnPlayer(pid, "set stronghold to " .. stronghold)
+end
+
+return Methods
